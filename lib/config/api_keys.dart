eb7255beb0713483157b6ff0f3f720b8b0190e8c
RUPESH KUMAR TIWARI <116380411+rupesh88899@users.noreply.github.com>
2024-07-25 02:49:07 +0530

    Update api_keys.dart
    /////// PLEASE USE YOUR OWN API KEY AND CONTEXTKEY 
    
    //links = context key ->    https://programmablesearchengine.google.com/controlpanel/create
    //link = apikey ->          https://developers.google.com/custom-search/v1/overview

--- lib/config/api_keys.dart
+++ lib/config/api_keys.dart
@@ -1,3 +1,8 @@
+
+/////// PLEASE USE YOUR OWN API KEY AND CONTEXTKEY 
+
+//links = context key ->    https://programmablesearchengine.google.com/controlpanel/create
+//link = apikey ->          https://developers.google.com/custom-search/v1/overview
 const String apiKey = "AIzaSyBrXkqB00RQ1pfY3brDp2rwBmsT7ccaFmY";
 
 const String contextKey = "968d949c6b75c47ae";
